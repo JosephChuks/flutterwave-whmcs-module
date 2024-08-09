b7b4cf0ffffffd241f9b47e8e46c31b94c3d54e8
Joseph Chuks <josephihemekwa@gmail.com>
2024-08-09 16:48:26 +0100

    Initial commit

---
+++ callback/flutterwave.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * Flutterwave WHMCS Payment Gateway Module - Callback
+ *
+ * This Payment Gateway module allows you to integrate Flutterwave payment solutions with the
+ * WHMCS platform.
+ *
+ * For more information, please refer to the online documentation: 
+ * https://developer.flutterwave.com/docs
+ * 
+ * 
+ * @author Joseph Chuks <info@josephchuks.com>
+ *
+ * @copyright Copyright (c) Joseph Chuks 2024
+ */
+
+
+// Require libraries needed for gateway module functions.
+require_once __DIR__ . '/../../../init.php';
+require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
+require_once __DIR__ . '/../../../includes/invoicefunctions.php';
+
+// Detect module name from filename.
+$gatewayModuleName = basename(__FILE__, '.php');
+
+// Fetch gateway configuration parameters.
+$gatewayParams = getGatewayVariables($gatewayModuleName);
+
+// Die if module is not active.
+if (!$gatewayParams['type']) {
+    die("Module Not Activated");
+}
+
+
+$invoiceId = $_GET["tx_ref"];
+$transactionId = $_GET["transaction_id"];
+$status = $_GET["status"];
+$secretKey = $gatewayParams['secretKey'];
+
+$systemUrl = $gatewayParams['systemurl'];
+$invoice_url = $systemUrl . 'viewinvoice.php?id=' . $invoiceId;
+
+if ($invoiceId && $transactionId && $status) {
+
+    if ($status === 'successful') {
+
+        // Check invoice
+        $invoiceId = checkCbInvoiceID($invoiceId, $gatewayParams['invoiceid']);
+
+        // Check Transaction
+        checkCbTransID($transactionId);
+
+        // Verify Transaction
+        $curl = curl_init();
+        $api = "https://api.flutterwave.com/v3/transactions/$transactionId/verify";
+
+        curl_setopt($curl, CURLOPT_URL, $api);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPGET, true);
+
+        $headers = [
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $secretKey,
+        ];
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+        $response = curl_exec($curl);
+
+        if (curl_errno($curl)) {
+            $result = new stdClass();
+            $result->status = 'error';
+            $result->message = 'cURL error: ' . curl_error($curl);
+        } else {
+            $result = json_decode($response);
+        }
+
+        curl_close($curl);
+
+
+        if ($result->status == 'success') {
+
+            // Convert to base currency
+            $paymentAmount = $result->data->amount;
+            if ($gatewayParams['convertto']) {
+                $result = select_query("tblclients", "tblinvoices.invoicenum,tblclients.currency,tblcurrencies.code", array("tblinvoices.id" => $invoiceId), "", "", "", "tblinvoices ON tblinvoices.userid=tblclients.id INNER JOIN tblcurrencies ON tblcurrencies.id=tblclients.currency");
+                $data = mysql_fetch_array($result);
+                $invoice_currency_id = $data['currency'];
+
+                $converto_amount = convertCurrency($paymentAmount, $gatewayParams['convertto'], $invoice_currency_id);
+                $amount = format_as_currency($converto_amount);
+            } else {
+                $amount = number_format(floatval($paymentAmount), 2, '.', '');
+            }
+
+            // Log Payment
+            if ($gatewayParams['gatewayLogs'] == 'on') {
+                $output = "Transaction ref: " . $transactionId
+                    . "\r\nInvoice ID: " . $invoiceId
+                    . "\r\nStatus: succeeded";
+                logTransaction($gatewayModuleName, $output, "Success");
+            }
+
+            // Add Invoice
+            addInvoicePayment(
+                $invoiceId,
+                $transactionId,
+                $amount,
+                0,
+                $gatewayModuleName
+            );
+        } else {
+
+            if ($gatewayParams['gatewayLogs'] == 'on') {
+                $output = "Transaction ref: " . $transactionId
+                    . "\r\nInvoice ID: " . $invoiceId
+                    . "\r\nStatus: failed";
+                logTransaction($gatewayModuleName, $output, "Failed");
+            }
+        }
+    } else {
+        if ($gatewayParams['gatewayLogs'] == 'on') {
+            $output = "Transaction ref: " . $transactionId
+                . "\r\nInvoice ID: " . $invoiceId
+                . "\r\nStatus: failed";
+            logTransaction($gatewayModuleName, $output, "Failed");
+        }
+    }
+
+    header('Location: ' . $invoice_url);
+} else {
+    header('Location: ' . $invoice_url);
+}
---
+++ flutterwave.php
@@ -0,0 +1,185 @@
+<?php
+
+/**
+ * Flutterwave WHMCS Payment Gateway Module
+ *
+ * This Payment Gateway module allows you to integrate Flutterwave payment solutions with the
+ * WHMCS platform.
+ *
+ * For more information, please refer to the online documentation: 
+ * https://developer.flutterwave.com/docs
+ * 
+ * 
+ * @author Joseph Chuks <info@josephchuks.com>
+ *
+ * @copyright Copyright (c) Joseph Chuks 2024
+ */
+
+
+
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}
+
+function flutterwave_MetaData()
+{
+    return array(
+        'DisplayName' => 'Flutterwave',
+        'APIVersion' => '3.0',
+        'DisableLocalCredtCardInput' => true,
+        'TokenisedStorage' => false,
+    );
+}
+
+function flutterwave_config()
+{
+    return array(
+        'FriendlyName' => array(
+            'Type' => 'System',
+            'Value' => 'Flutterwave',
+        ),
+
+        'cBname' => array(
+            'FriendlyName' => 'Flutterwave Business Name',
+            'Type' => 'text',
+            'Size' => '50',
+            'Default' => '',
+            'Description' => 'Enter business name here',
+        ),
+
+        'cBdescription' => array(
+            'FriendlyName' => 'Business Description',
+            'Type' => 'text',
+            'Size' => '50',
+            'Default' => '',
+            'Description' => 'Enter business description here',
+        ),
+
+        'whmcsLogo' => array(
+            'FriendlyName' => 'Icon',
+            'Type' => 'text',
+            'Size' => '80',
+            'Default' => '',
+            'Description' => 'Enter the link to your site icon (square)',
+        ),
+
+        'publicKey' => array(
+            'FriendlyName' => 'Public Key',
+            'Type' => 'text',
+            'Size' => '50',
+            'Default' => '',
+            'Description' => 'Enter public key here',
+        ),
+        'secretKey' => array(
+            'FriendlyName' => 'Secret Key',
+            'Type' => 'text',
+            'Size' => '50',
+            'Default' => '',
+            'Description' => 'Enter secret key here',
+        ),
+
+        'payButtonText' => array(
+            'FriendlyName' => 'Pay Button Text',
+            'Type' => 'text',
+            'Size' => '25',
+            'Default' => 'Pay Now',
+            'Description' => 'Text to display on your payment button',
+        ),
+
+        'gatewayLogs' => array(
+            'FriendlyName' => 'Gateway logs',
+            'Type' => 'yesno',
+            'Description' => 'Select to enable gateway logs',
+            'Default' => '0'
+        ),
+    );
+}
+
+
+function flutterwave_link($params)
+{
+
+    // Gateway Configuration Parameters
+    $publicKey = $params['publicKey'];
+    $secretKey = $params['secretKey'];
+    $companyName = $params['cBname'];
+    $companyDescription = $params['cBdescription'];
+    $logo = $params['whmcsLogo'];
+    $payButtonText = $params['payButtonText'];
+
+    // Invoice Parameters
+    $invoiceId = $params['invoiceid'];
+    $description = $params["description"];
+    $amount = $params['amount'];
+    $currencyCode = $params['currency'];
+
+    // Client Parameters
+    $firstname = $params['clientdetails']['firstname'];
+    $lastname = $params['clientdetails']['lastname'];
+    $email = $params['clientdetails']['email'];
+    $phone = $params['clientdetails']['phonenumber'];
+
+    // System Parameters
+    $systemUrl = $params['systemurl'];
+    $langPayNow = $params['langpaynow'];
+    $moduleName = $params['paymentmethod'];
+    $returnUrl = "'.$systemUrl.'viewinvoice.php?id='.$invoiceId.'";
+    $redirectUrl = $systemUrl . 'modules/gateways/callback/' . $moduleName . '.php';
+
+
+    $htmlOutput = '<script src="https://checkout.flutterwave.com/v3.js"></script>';
+    $htmlOutput .= '<form>';
+    $htmlOutput .= '<button type="button" id="start-payment-button" 
+    style="cursor: pointer;
+    position: relative;
+    background-color: #ff9b00;
+    color: #12122c;
+    max-width: 100%;
+    padding: 7.5px 16px;
+    font-weight: 500;
+    font-size: 14px;
+    border-radius: 4px;
+    border: none;
+    transition: all .1s ease-in;
+    vertical-align: middle;" onclick="makePayment()">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <rect x="3" y="6" width="18" height="12" rx="2" ry="2" />
+  <line x1="3" y1="10" x2="21" y2="10" />
+  <line x1="3" y1="14" x2="21" y2="14" />
+</svg>&nbsp;
+' . $langPayNow . '
+    </button>';
+    $htmlOutput .= '</form>';
+    $htmlOutput .= '<script>
+          function makePayment() {
+            FlutterwaveCheckout({
+              public_key: "' . $publicKey . '",
+              tx_ref: "' . $invoiceId . '",
+              amount: ' . $amount . ',
+              currency: "' . $currencyCode . '",
+              payment_options: "card, ussd, banktransfer, account, internetbanking, nqr, applepay, googlepay, enaira, opay",
+              meta: {
+                consumer_id: "' . $invoiceId . '",
+                consumer_mac: "' . $_SERVER['REMOTE_ADDR'] . '",
+              },
+              redirect_url: "' . $redirectUrl . '",
+              customer: {
+                email: "' . $email . '",
+                phone_number: "' . $phone . '",
+                name: "' . $firstname . ' ' . $lastname . '",
+              },
+              customizations: {
+                title: "' . $companyName . '",
+                description: "Online Payment: ' . $description . '",
+                logo: "' . $logo . '",
+              },
+              onclose: function() {
+                window.location.href = "' . $systemUrl . 'viewinvoice.php?id=' . $invoiceId . '";
+              }
+            });
+          }
+        </script>';
+
+
+    return $htmlOutput;
+}
