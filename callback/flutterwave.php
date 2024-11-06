8963133abfb2220e602647988b23257d12b6672a
Joseph Chuks <josephihemekwa@gmail.com>
2024-11-06 19:34:57 +0100

    Update flutterwave.php
    Fixed invoice not getting marked for successfull transactions

--- callback/flutterwave.php
+++ callback/flutterwave.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Flutterwave WHMCS Payment Gateway Module - Callback
+ * Flutterwave WHMCS Payment Gateway Module - Callback v1.2.1
  *
  * This Payment Gateway module allows you to integrate Flutterwave payment solutions with the
  * WHMCS platform.
@@ -33,91 +33,92 @@ if (!$gatewayParams['type']) {
 }
 
 
-$invoiceId = $_GET["id"];
-$transactionId = $_GET["id"];
+$invoiceId = $_GET["tx_ref"];
+$trxStatus = $_GET["status"];
+$transactionId = $_GET["transaction_id"];
 $secretKey = $gatewayParams['secretKey'];
 
 $systemUrl = $gatewayParams['systemurl'];
 $invoice_url = $systemUrl . 'viewinvoice.php?id=' . $invoiceId;
 
-if ($invoiceId) {
+// Check invoice
+$invoiceId = checkCbInvoiceID($invoiceId, $gatewayParams['invoiceid']);
 
-    // Check invoice
-    $invoiceId = checkCbInvoiceID($invoiceId, $gatewayParams['invoiceid']);
+// Check Transaction
+checkCbTransID($transactionId);
 
-    // Check Transaction
-    checkCbTransID($transactionId);
+// Verify Transaction
+$curl = curl_init();
+$api = "https://api.flutterwave.com/v3/transactions/$transactionId/verify";
 
-    // Verify Transaction
-    $curl = curl_init();
-    $api = "https://api.flutterwave.com/v3/transactions/$transactionId/verify";
+curl_setopt($curl, CURLOPT_URL, $api);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPGET, true);
 
-    curl_setopt($curl, CURLOPT_URL, $api);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPGET, true);
+$headers = [
+    'Content-Type: application/json',
+    'Authorization: Bearer ' . $secretKey,
+];
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-    $headers = [
-        'Content-Type: application/json',
-        'Authorization: Bearer ' . $secretKey,
-    ];
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+$response = curl_exec($curl);
 
-    $response = curl_exec($curl);
+if (curl_errno($curl)) {
+    $result = new stdClass();
+    $result->status = 'error';
+    $result->message = 'cURL error: ' . curl_error($curl);
+} else {
+    $result = json_decode($response);
+}
 
-    if (curl_errno($curl)) {
-        $result = new stdClass();
-        $result->status = 'error';
-        $result->message = 'cURL error: ' . curl_error($curl);
+curl_close($curl);
+
+
+if ($result->status == 'success') {
+
+    // Convert to base currency
+    $paymentAmount = $result->data->amount;
+    if ($gatewayParams['convertto']) {
+        $result = select_query("tblclients", "tblinvoices.invoicenum,tblclients.currency,tblcurrencies.code", array("tblinvoices.id" => $invoiceId), "", "", "", "tblinvoices ON tblinvoices.userid=tblclients.id INNER JOIN tblcurrencies ON tblcurrencies.id=tblclients.currency");
+        $data = mysql_fetch_array($result);
+        $invoice_currency_id = $data['currency'];
+
+        $converto_amount = convertCurrency($paymentAmount, $gatewayParams['convertto'], $invoice_currency_id);
+        $amount = format_as_currency($converto_amount);
     } else {
-        $result = json_decode($response);
+        $amount = number_format(floatval($paymentAmount), 2, '.', '');
     }
 
-    curl_close($curl);
-
-
-    if ($result->status == 'success') {
-
-        // Convert to base currency
-        $paymentAmount = $result->data->amount;
-        if ($gatewayParams['convertto']) {
-            $result = select_query("tblclients", "tblinvoices.invoicenum,tblclients.currency,tblcurrencies.code", array("tblinvoices.id" => $invoiceId), "", "", "", "tblinvoices ON tblinvoices.userid=tblclients.id INNER JOIN tblcurrencies ON tblcurrencies.id=tblclients.currency");
-            $data = mysql_fetch_array($result);
-            $invoice_currency_id = $data['currency'];
-
-            $converto_amount = convertCurrency($paymentAmount, $gatewayParams['convertto'], $invoice_currency_id);
-            $amount = format_as_currency($converto_amount);
-        } else {
-            $amount = number_format(floatval($paymentAmount), 2, '.', '');
-        }
-
-        // Log Payment
-        if ($gatewayParams['gatewayLogs'] == 'on') {
-            $output = "Transaction ref: " . $transactionId
-                . "\r\nInvoice ID: " . $invoiceId
-                . "\r\nStatus: succeeded";
-            logTransaction($gatewayModuleName, $output, "Success");
-        }
-
-        // Add Invoice
-        addInvoicePayment(
-            $invoiceId,
-            $transactionId,
-            $amount,
-            0,
-            $gatewayModuleName
-        );
-    } else {
-
-        if ($gatewayParams['gatewayLogs'] == 'on') {
-            $output = "Transaction ref: " . $transactionId
-                . "\r\nInvoice ID: " . $invoiceId
-                . "\r\nStatus: failed";
-            logTransaction($gatewayModuleName, $output, "Failed");
-        }
+    // Log Payment
+    if ($gatewayParams['gatewayLogs'] == 'on') {
+        $output = "Transaction ref: " . $transactionId
+            . "\r\nInvoice ID: " . $invoiceId
+            . "\r\nStatus: succeeded";
+        logTransaction($gatewayModuleName, $output, "Success");
     }
 
+    // Add Invoice
+    addInvoicePayment(
+        $invoiceId,
+        $transactionId,
+        $amount,
+        0,
+        $gatewayModuleName
+    );
 
     header('Location: ' . $invoice_url);
 } else {
-    header('Location: ' . $invoice_url);
+
+    if ($gatewayParams['gatewayLogs'] == 'on') {
+        $output = "Transaction ref: " . $transactionId
+            . "\r\nInvoice ID: " . $invoiceId
+            . "\r\nStatus: failed";
+        logTransaction($gatewayModuleName, $output, "Failed");
+    }
+
+    echo
+    '<script type="text/javascript">
+    alert("Transaction failed. Please try again");
+    window.location.href = "' . $invoice_url . '";
+    </script>';
 }
